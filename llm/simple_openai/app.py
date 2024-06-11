ce632babbe1e2b9e247584820c8215bc965430a2
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2024-06-11 21:33:41 +0200

    refactor: minor modifications to clarify what the code is doing

--- llm/simple_openai/app.py
+++ llm/simple_openai/app.py
@@ -1,16 +1,23 @@
 from openai import OpenAI
 from dotenv import dotenv_values
+import os
 
 config = next((dotenv_values(f"{p}env.txt") for p in ('', '../', '../../') if os.path.exists(f"{p}env.txt")), {})
 client = OpenAI(api_key=config["OPENAI_API_KEY"])
 
+system="You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+user="Compose a poem that explains the concept of recursion in programming."
+
+print("System message:")
+print(system)
+print("User message:")
+print(user)
+
 completion = client.chat.completions.create(
   model="gpt-4",
   messages=[
-    {"role": "system", 
-     "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": 
-     "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": system},
+    {"role": "user", "content": user}
   ]
 )
 
