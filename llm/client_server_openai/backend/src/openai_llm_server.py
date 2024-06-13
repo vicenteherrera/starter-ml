02e8999d6a417fa8567acb6114e3370221f3e57e
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2024-06-13 18:37:57 +0200

    feat: separated directories for client/server example

--- llm/client_server_openai/backend/src/openai_llm_server.py
+++ llm/client_server_openai/backend/src/openai_llm_server.py
@@ -1,38 +1,30 @@
 import os
-import openai
-import langchain
 from dotenv import load_dotenv
-from langchain.llms import OpenAI
-
+from langchain_openai import OpenAI
 from flask import Flask, request, jsonify
+
+from dotenv import dotenv_values
 
 app = Flask(__name__)
 
-load_dotenv()
+config = next((dotenv_values(f"{p}env.txt") for p in ('', '../', '../../', '../../../') if os.path.exists(f"{p}env.txt")), {})
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(api_key=config["OPENAI_API_KEY"])
 
 def invoke(prompt):
-
-    llm = OpenAI() # instantiate the model
-    
     response = llm(prompt) # invoke OpenAI LLM
-
     print(response) # output the response
-    
     return response
 
 
 # Exposing the endpoint 
 @app.route('/ask', methods=['POST'])
 def ask():
-    
     # Fetch the question from the user's request
     prompt = request.json.get('question')
-
     # Invoke the model function 
     response = invoke(prompt)
-
     # Return response
     return jsonify({"answer": response})
 
--- llm/client_server_openai/frontend/src/openai_llm_client.py
+++ llm/client_server_openai/frontend/src/openai_llm_client.py
@@ -1,13 +1,7 @@
-import os
 import streamlit as st
 import requests
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv()
-
-# Initialize OpenAI API key
-openai_api_key = os.getenv("OPENAI_API_KEY")
 
 SERVER_URL = "http://127.0.0.1:3000/ask"
 
@@ -17,7 +11,6 @@ user_question = st.text_input("Ask your question:")
 
 # Function to ask user a question and get an answer from the microservice
 def ask_user():
-
     if user_question:
         # Call the Flask API endpoint to get the answer
         response = requests.post(
