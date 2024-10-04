38842e30f3f103be750331079d2730306f514b67
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2024-10-04 16:43:38 +0200

    fix: cache quotation windows

--- llm/simple_local_model/scripts/run-container.ps1
+++ llm/simple_local_model/scripts/run-container.ps1
@@ -1,10 +1,12 @@
 
-MODEL="TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-MODEL_NAME="TinyLlama--TinyLlama-1.1B-Chat-v1.0"
-SNAPSHOT="fe8a4ea1ffedaf415f4da2f062534de366a451e6"
-MODEL_DIR="./.cache/huggingface/hub/models--${MODEL_NAME}/snapshots/$SNAPSHOT"
+$MODEL="TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+$MODEL_NAME="TinyLlama--TinyLlama-1.1B-Chat-v1.0"
+$SNAPSHOT="fe8a4ea1ffedaf415f4da2f062534de366a451e6"
+$MODEL_DIR="./.cache/huggingface/hub/models--${MODEL_NAME}/snapshots/$SNAPSHOT"
+$CONTAINER_IMAGE="quay.io/vicenteherrera/test-llm"
+$CACHE="$env:USERPROFILE\.cache"
 
 docker run -it `
 	-e MODEL_DIR="${MODEL_DIR}" `
-    -v "$env:USERPROFILE\.cache":/app/.cache:ro `
+	-v ${CACHE}:/app/.cache:ro `
 	${CONTAINER_IMAGE}:no-model
